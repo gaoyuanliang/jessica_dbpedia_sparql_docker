649462097d0905e7abe66b40f5eff0160df3fe1d
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2020-10-27 20:59:49 +0400

    Update and rename dbpedia_data_filtering.py to jessica_dbpedia_data_filtering.py

--- jessica_dbpedia_data_filtering.py
+++ jessica_dbpedia_data_filtering.py
@@ -1,4 +1,4 @@
-#############dbpedia_data_filtering.py#############
+#############jessica_dbpedia_data_filtering.py#############
 '''
 https://github.com/tyrex-team/sparqlgx
 '''
@@ -16,7 +16,6 @@ sqlContext = SparkSession.builder.getOrCreate()
 '''
 load the data
 '''
-
 schema = StructType()\
 	.add("subject",StringType(),True)\
 	.add("relation",StringType(),True)\
@@ -27,6 +26,7 @@ mappingbased_objects_en = sqlContext.read.format('csv')\
 	.schema(schema)\
 	.load('mappingbased_objects_en.ttl')
 mappingbased_objects_en.registerTempTable('mappingbased_objects_en')
+
 
 '''
 filter according to the relation
@@ -151,7 +151,6 @@ sqlContext.sql(u"""
 	escapechar="\\",
 	sep="\t")
 
-
 '''
 sqlContext.sql(u"""
 	SELECT relation, COUNT(*) AS triplet_count
@@ -168,4 +167,57 @@ sqlContext.sql(u"""
 	""").show(1000, False)
 '''
 
-#############dbpedia_data_filtering.py#############
+
+schema = StructType()\
+	.add("subject",StringType(),True)\
+	.add("relation",StringType(),True)\
+	.add("object",StringType(),True)
+
+instance_types_en = sqlContext.read.format('csv')\
+	.options(delimiter=' ')\
+	.schema(schema)\
+	.load('/jessica/instance_types_en.ttl')
+instance_types_en.registerTempTable('instance_types_en')
+
+sqlContext.sql(u"""
+	SELECT *
+	FROM instance_types_en
+	WHERE object LIKE "%dbpedia%org%ontology%"
+	""").write.mode("Overwrite").json("/jessica/instance_types_en")
+instance_types_en = sqlContext.read.json("/jessica/instance_types_en")
+instance_types_en.registerTempTable("instance_types_en")
+
+sqlContext.sql(u"""
+	SELECT t.* 
+	FROM instance_types_en as t
+	JOIN entity_count2 AS e
+	ON e.entity = t.subject
+	""").write.mode("Overwrite").json("instance_types_en1")
+sqlContext.read.json("instance_types_en1").registerTempTable("instance_types_en1")
+
+sqlContext.sql(u"""
+	SELECT subject, 
+	collect_set(relation)[0] as relation,
+	collect_set(object)[0] as object
+	from instance_types_en1
+	group by subject
+	""").write.mode("Overwrite").json("instance_types_en2")
+
+sqlContext.read.json("instance_types_en2").registerTempTable("instance_types_en2")
+
+'''
+save to the ttl file
+'''
+sqlContext.sql(u"""
+	SELECT CONCAT(subject, ' ', relation, ' ', object, ' . ')
+	FROM instance_types_en2
+	""").toPandas().to_csv(
+	"/jessica/instance_types_en_small.ttl",
+	header = False,
+	index = False,
+	quoting = csv.QUOTE_NONE,
+	quotechar="",  
+	escapechar="\\",
+	sep="\t")
+
+#############jessica_dbpedia_data_filtering.py#############
